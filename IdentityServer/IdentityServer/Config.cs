5bca1f639a3836f940eae55b4a0f093cfb7ae288
John Markham <seanomarachain@gmail.com>
2020-08-24 21:22:20 +0100

    Initial version of test service

---
+++ IdentityServer/IdentityServer/Config.cs
@@ -0,0 +1,62 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("myresourceapi", "My Resource API")
+                {
+                    Scopes = {
+                        new Scope("apiscope"),
+                        new Scope("DALApi1"),
+                        new Scope("otherscope")
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                // for public api
+                new Client
+                {
+                    ClientId = "PegaClient",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("ShareASecretWithIDS4".Sha256())
+                    },
+                    AllowedScopes = { "apiscope", "DALApi1" }
+                },
+                new Client
+                {
+                    ClientId = "Alice",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret_alice_password".Sha256())
+                    },
+                    
+                    AllowedScopes = { "apiscope" }
+                },
+                new Client
+                {
+                    ClientId = "Bob",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret_bob_password".Sha256())
+                    },
+                    AllowedScopes = { "apiscope" }
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServer/microservice/Controllers/WFController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace microservice.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WFController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WFController> _logger;
+
+        public WFController(ILogger<WFController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        // GET: /api/WF/1
+        [Authorize(Policy = "AuthClient")]
+        [HttpGet("{id}")]
+        public IEnumerable<WeatherFing> GetId()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherFing
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+        }
+
+        // GET: /api/WF
+        [Authorize(Policy = "PublicSecure")]
+        [HttpGet]
+        public IEnumerable<WeatherFing> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherFing
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+            
+        }
+
+        // POST: /api/file/WF
+        
+        [HttpPost("file")]
+        public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size });
+        }
+
+    }
+}
